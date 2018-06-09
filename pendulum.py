f68516c4259b15d039e3b4a2017f730e17ff1b5b
unknown <kylejlynch@gmail.com>
2018-06-09 15:17:11 -0500

    animation

--- pendulum.py
+++ pendulum.py
@@ -3,9 +3,10 @@ import numpy as np
 from numpy import sin, cos
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # Equations
-def equations(y0, t) :
+def equations(y0, time) :
     theta, x = y0
     f = [x,-c*x -(g/l) * sin(theta)]
     return f
@@ -41,3 +42,26 @@ theta2 = [theta0 * cos(w*t) for t in time]
 
 # plot results
 plot_results(time,theta1,theta2)
+y=theta1[:,0]
+y2=theta2
+
+fig, (ax1, ax2) = plt.subplots(2,1,sharey=True)
+s = ('Initial Angle = ' + str(initial_angle) + ' degrees')
+plt.suptitle('Pendulum Motion: ' + s)
+line, = ax1.plot(time, y)
+line2, = ax2.plot(time, y2)
+plt.xlabel('Time (s)')
+fig.text(0.04, 0.5, 'Angle (rad)', va='center', rotation='vertical')
+ax1.set_ylim(-1.2,1)
+ax1.legend(['nonlinear, damped'], loc='lower right')
+ax2.legend(['linear, undamped'], loc='lower right')
+plt.subplots_adjust(top=0.94)
+
+def update(num, time, y, y2, line,line2):
+    line.set_data(time[:num], y[:num])
+    line2.set_data(time[:num], y2[:num])
+    return line, line2
+
+ani = animation.FuncAnimation(fig, update, len(time), fargs=[time, y,y2, line,line2],
+                              interval=10, blit=True)
+plt.show()
